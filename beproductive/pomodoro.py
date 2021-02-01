6a1b74ab18c09c3a5eea125ff0d65dbb522941bc
Johannes Stutz <61063548+JohannesStutz@users.noreply.github.com>
2021-02-01 15:19:54 +0100

    fix pomodoro

--- beproductive/pomodoro.py
+++ beproductive/pomodoro.py
@@ -4,9 +4,28 @@ __all__ = ['WORK', 'BREAK', 'POMODOROS']
 
 # Cell
 from time import sleep
-import beproductive.blocker
+from .blocker import Blocker
 import sys
 
 WORK = 25
 BREAK = 5
 POMODOROS = 4
+
+# Cell
+if __name__=='__main__':
+    blocker = Blocker()
+    try: mode = sys.argv[1]
+    except: mode = 'stop'
+    if mode == 'start':
+        turn = 1
+        while turn <= POMODOROS:
+            blocker.block()
+            blocker.notify(f"Pomodoro no. {turn} started, work for {WORK} minutes")
+            sleep(WORK*60)
+            blocker.unblock()
+            if turn < POMODOROS:
+                blocker.notify(f"Pomodoro no. {turn} ended, take a {BREAK} minutes break")
+                sleep(BREAK*60)
+            else:
+                blocker.notify(f"Pomodoro session ended, take a longer break. All websites unblocked.", duration=10)
+            turn += 1
